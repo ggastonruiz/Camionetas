8c3875eb82957b2268bba6a805e1ef7bda120722
ggastonruiz <135028423+ggastonruiz@users.noreply.github.com>
2023-08-10 10:51:33 -0300

    Add files via upload

---
+++ scripst.js
@@ -0,0 +1,11 @@
+function teGustanLasCamionetas() {
+
+    alert("Te gusta las camionetas?")
+
+    if (confirm("Te gusta la RAPTOR?")) {
+        alert("Muy buena eleccion")
+    }
+    else {
+        alert("Mala eleccion")
+    }
+}
